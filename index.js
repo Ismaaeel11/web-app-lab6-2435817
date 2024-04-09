21a076e21e771dbdd6cce4533e9d9baa4ad86b07
Ismaaeel11 <2435817@students.wits.ac.za>
2024-04-09 15:40:31 +0200

    try4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-
+const PORT = process.env.PORT || 3001; 
 app.use(express.json());
 const path = require('path');
 app.use(express.static(__dirname));
